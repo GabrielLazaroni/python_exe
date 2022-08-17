e463a05c28b1f244f288fcd3507be032bf8ac0fd
Lazaroni <gabriellazaroni@outlook.com.br>
2022-08-16 22:09:25 -0300

    more exe

--- PythonExercise/exe110/main.py
+++ PythonExercise/exe110/main.py
@@ -1,8 +1,4 @@
 import calculos
 
 num = float(input('Digite um valor: R$'))
-porc = int(input('Digite a porcentagem: '))
-print(f'{porc}% a mais no valor de {calculos.moeda(num)} é {calculos.aumentar(num, porc, True)}')
-print(f'Com desconto de {porc}% no valor de {calculos.moeda(num)}, o valor da compra é de {calculos.diminuir(num, porc, True)}')
-print(f'o dobro de {calculos.moeda(num)} é {calculos.dobro(num, True)}')
-print(f'A metade de {calculos.moeda(num)} é {calculos.metade(num, True)}')
+calculos.resumo(num)
